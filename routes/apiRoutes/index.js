c0c3800eeac34789b3fad294ccd8be033bbefb1a
Mallory <mallory.faria@gmail.com>
2021-02-21 17:44:51 -0500

    fixed issue with writing to db

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,26 +3,27 @@ const path = require('path');
 const router = require('express').Router();
 const notes = require('../../db/db.json');
 
+
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
     let noteText = req.body;
   
     if (!noteText) {
       res.status(400).send('The note is empty.');
     } else {
-      const note = notes.push(noteText);
+
+      notes.push(noteText);
       fs.writeFileSync(
         path.join(__dirname, '../../db/db.json'),
         JSON.stringify(noteText)
       );
-      res.json(note);
 
+      res.json(noteText);
     }
   });
+
 
 module.exports = router;
