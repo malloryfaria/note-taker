852b26d4e531769a75df3a133054870b9ca648d8
Mallory <mallory.faria@gmail.com>
2021-02-21 14:48:26 -0500

    added initial server.js file

---
+++ server.js
@@ -0,0 +1,27 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const { notes } = require('./db/db.json');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });  
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
