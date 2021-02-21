bc9140d06d2534f520f56d1a28f0ab02ba157328
Mallory <mallory.faria@gmail.com>
2021-02-21 18:31:57 -0500

    finalizing

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,27 +3,26 @@ const path = require('path');
 const router = require('express').Router();
 const notes = require('../../db/db.json');
 
+const noteArray = [];
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
-    let noteText = req.body;
-  
-    if (!noteText) {
-      res.status(400).send('The note is empty.');
-    } else {
+  let noteText = req.body;
 
-      notes.push(noteText);
-      fs.writeFileSync(
-        path.join(__dirname, '../../db/db.json'),
-        JSON.stringify(noteText)
-      );
-
-      res.json(noteText);
-    }
-  });
+  if (!noteText) {
+    res.status(400).send('The note is empty.');
+  } else {
+    noteArray.push(noteText);
+    res.json(notes);
+    fs.writeFileSync(
+      path.join(__dirname, '../../db/db.json'),
+      JSON.stringify(noteArray)
+    );
+  }
+});
 
 
 module.exports = router;
