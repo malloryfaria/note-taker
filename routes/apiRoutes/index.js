bf76a89828da0b23924f5fc0c5856a3e264535e8
Mallory <mallory.faria@gmail.com>
2021-02-21 16:34:12 -0500

    added id to post

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -7,6 +7,8 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
     let noteText = req.body;
   
     if (!noteText) {
